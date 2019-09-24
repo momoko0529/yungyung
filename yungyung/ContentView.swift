42b95e55954ff8c77a5c560c001963fd71dbf028
user21 <osbert5618841@gmail.com>
2019-09-24 16:42:15 +0800

    Initial Commit

---
+++ yungyung/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yungyung
+//
+//  Created by User21 on 2019/9/24.
+//  Copyright © 2019 CHEN-YU-HAO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
